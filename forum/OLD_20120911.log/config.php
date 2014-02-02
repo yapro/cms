37a417e7d1241960723a43567435b2d94c5f154e
lebnik <totx@ya.ru>
2014-02-02 09:56:32 +0400

    Added .gitignore rules

--- forum/OLD_20120911.log/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(stristr($_SERVER['REQUEST_URI'], '//')){ header('location: http://'. $_SERVER['HTTP_HOST'].'/404'); exit; }// хак против неправильных урл
-$acm_type = 'file';
-$load_extensions = '';
-$GLOBALS['system_include'] = true;
-include_once($_SERVER['DOCUMENT_ROOT'].'/index.php');
-$table_prefix = F;// префикс таблиц форума
-?>
