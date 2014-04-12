1cd1c796ea02b09eefc3cc34f01052fe8b6b1202
Lebnik <totx@yandex.ru>
2014-04-12 09:12:46 +0400

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR);
+error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR | E_USER_NOTICE);
 $dbms = 'mysql';
 $dbhost = 'you_host';
 $dbport = '';
